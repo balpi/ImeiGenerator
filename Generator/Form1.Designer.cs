53f65255d6c10b77febdfacf33a782e205ab4a11
balpi <serhat_balpetek@hotmail.com>
2022-11-28 22:36:29 +0300

    Add project files.

---
+++ Generator/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Copyitem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(17, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 80);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GENERATE FOR VERIZON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(17, 50);
+            this.textBox2.MaxLength = 14;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 30);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number Starts With";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "IMEI NUMBER";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 80);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "GENERATE FOR AT&T";
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(356, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(166, 228);
+            this.listBox1.TabIndex = 6;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(240, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(101, 21);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Generate 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(240, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(109, 21);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Generate 10";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(240, 95);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(109, 21);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Generate 25";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(417, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 10;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Copyitem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 56);
+           
+            // 
+            // Copyitem
+            // 
+            this.Copyitem.Name = "Copyitem";
+            this.Copyitem.Size = new System.Drawing.Size(210, 24);
+            this.Copyitem.Text = "Copy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 266);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "IMEI GENERATOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Copyitem;
+    }
+}
+
---
+++ Generator/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Generator
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        byte lop;
+        bool numberexist = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "";
+            
+            listBox1.Visible = false;
+            listBox1.Items.Clear();
+            if (radioButton1.Checked)
+            {
+                lop = 1;
+                label2.Text = "IMEI NUMBER VERIZON";
+            }
+            if (radioButton2.Checked)
+            {
+                lop = 10;
+                label3.Text = "Verizon";
+            }
+            if (radioButton3.Checked)
+            {
+                lop = 25;
+                label3.Text = "Verizon";
+            }
+            string number="";
+            for (int i = 0; i < lop; i++)
+            {
+
+
+                int sumtry = 0;
+                while (numberexist)
+                {
+                    number = imei_Creator();
+                    numberexist = checknumber(number);
+                    sumtry = sumtry + 1;
+                    if (sumtry == 100)
+                    {
+                        MessageBox.Show( "I have tried 100 different combination but could not find a new number. Please change the Starting Numbers and try again", "Warning--Change Starting Numbers");
+                        return;
+                    }
+                }
+                numberexist = true;
+                if (lop > 1)
+                {
+                    listBox1.Visible = true;
+                    listBox1.Items.Add(number);
+                }
+                else
+                {
+                    textBox1.Text = number;
+                }
+                
+                TextWriter wt = new StreamWriter("numbers.txt", true);
+                wt.WriteLine(number+" Verizon");
+                wt.Close();
+            }
+        }
+        private string imei_Creator()
+        {
+            string a="", b="";
+            
+            int x=0,z=0,sum=0;
+
+            x = textBox2.Text.Length;
+          int  y = 14 - x;
+            string[] c= new string[y];
+            
+            for (int i = 0; i < y; i++)
+            {
+                if (x > 14)
+                {
+                    break;
+                }
+                c[i] = (rnd.Next(0, 9)).ToString();
+
+            }
+            a = textBox2.Text;
+            for (int j = 0; j < c.Length; j++)
+            {
+
+                a = a + c[j];
+            }
+
+            for (int k = 0; k < a.Length; k++)
+            {
+                b = a.Substring(k, 1);
+                z = Convert.ToInt32(b);
+                if ((k+1) / 2m == (k+1) / 2)
+                {
+                    z = z * 2;
+
+                    foreach(char d in z.ToString())
+                    {
+                        sum = sum + Convert.ToInt32(z);
+                    }
+                }
+                else
+                {
+                    sum = sum + z;
+                }
+              
+            }
+
+            int kk = 10 - (sum- ((sum / 10)*10));
+            if (kk == 10)
+            {
+                kk = 0;
+            }
+            a = a + kk.ToString();
+
+
+
+
+            //button1.Text = a.Length.ToString();
+            rnd.Next();
+            return a;
+            //357772175770817
+        }
+        private bool checknumber(string a)
+        {
+            string number;
+            numberexist = false;
+            var fexist = File.Exists("numbers.txt");
+            if (fexist)
+            {
+                StreamReader rw = new StreamReader("numbers.txt");
+                while ((number = rw.ReadLine()) != null)
+                {
+                    if (number.IndexOf(a)>-1)
+                    {
+                        numberexist = true;
+                    }
+                }
+                rw.Close();
+            }
+            return numberexist;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Visible = false;
+
+            contextMenuStrip1.MouseClick += new MouseEventHandler(clicked);
+            listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.List_RightClick);
+
+        }
+        private void List_RightClick(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Right)
+            {
+                int index = this.listBox1.IndexFromPoint(e.Location);
+                if (index != ListBox.NoMatches)
+                {
+                   listBox1.SelectedIndex =index;
+                }
+            }
+
+        }
+        private void clicked(object sender, MouseEventArgs e)
+        {
+           
+            string a = listBox1.SelectedItem.ToString();
+            Clipboard.SetText (a);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            label3.Text = "";
+            listBox1.Visible = false;
+            listBox1.Items.Clear();
+            if (radioButton1.Checked)
+            {
+                lop = 1;
+                label2.Text = "IMEI NUMBER AT&T";
+            }
+            if (radioButton2.Checked)
+            {
+                lop = 10;
+                label3.Text = "AT&T";
+            }
+            if (radioButton3.Checked)
+            {
+                lop = 25;
+                label3.Text = "AT&T";
+            }
+            string number = "";
+            for (int i = 0; i < lop; i++)
+            {
+
+
+                int sumtry = 0;
+                while (numberexist)
+                {
+                    number = imei_Creator();
+                    numberexist = checknumber(number);
+                    sumtry = sumtry + 1;
+                    if (sumtry == 100)
+                    {
+                        MessageBox.Show("Warning--Change Starting Numbers", "I have tried 100 different combination but could not find a new number. Please change the Starting Numbers and try again");
+                        return;
+                    }
+                }
+                numberexist = true;
+                if (lop > 1)
+                {
+                    listBox1.Visible = true;
+                    listBox1.Items.Add(number);
+                }
+                else
+                {
+                    textBox1.Text = number;
+                }
+
+                TextWriter wt = new StreamWriter("numbers.txt", true);
+                wt.WriteLine(number + " AT&T");
+                wt.Close();
+            }
+        }
+
+        
+    }
+}
